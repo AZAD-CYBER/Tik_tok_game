89b8014b95a75f91171c3243d13de5de6ef37c8d
Azad Ansari <79094390+AZAD-CYBER@users.noreply.github.com>
2022-10-01 23:33:57 +0530

    History part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Board from "../components/Board";
 import { calculateWinner } from "./helper";
+import History from "../components/History";
 import "./styles/root.scss";
 function App() {
   const [history, setHistory] = useState([
@@ -38,12 +39,16 @@ function App() {
 
     setCurrentMove((prev) => prev + 1);
   };
+  const moveTo = (move) => {
+    setCurrentMove(move);
+  };
 
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <h2>{message}</h2>
       <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 }
